f1f94668b9e46e76f35c3184544241269b203b6c
ishu-k <54646524+ishu-k@users.noreply.github.com>
2020-03-07 23:31:20 +0000

    Update RuncukeTest.java

--- src/test/java/com/bdd/RuncukeTest.java
+++ src/test/java/com/bdd/RuncukeTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\ishuk\\IdeaProjects\\bddproject\\src\\test\\resouces")
+@CucumberOptions(features = "C:\\Users\\ishuk\\IdeaProjects\\bddproject\\src\\test\\resouces", plugin = {"pretty","json:target/cucumber.json","html:target/site/cucumber-html"})
 public class RuncukeTest {
 }

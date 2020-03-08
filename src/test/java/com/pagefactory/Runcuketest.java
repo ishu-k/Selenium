8af404d5a8adba42542c2864af68e1a3653245ec
ishu-k <54646524+ishu-k@users.noreply.github.com>
2020-03-08 00:46:00 +0000

    Update Runcuketest.java

--- src/test/java/com/pagefactory/Runcuketest.java
+++ src/test/java/com/pagefactory/Runcuketest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 //@CucumberOptions( features = .)
-@CucumberOptions(features = "C:\\Users\\ishuk\\IdeaProjects\\pagefactory\\src\\test\\resources",plugin = {"json:target/cucumber.json"})
+@CucumberOptions(features = "C:\\Users\\ishuk\\IdeaProjects\\pagefactory\\src\\test\\resources",plugin = {""pretty","json:target/cucumber.json","html:target/site/cucumber-html"})
 public class Runcuketest {
 
 }
